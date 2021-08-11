c500b2ed4edbf3aca23dab3d5a84688f5871ab22
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-08-11 21:06:04 +0100

    UPDATED

--- script.js
+++ script.js
@@ -28,38 +28,68 @@ function searchDeezer(query) {
 }
 
 function singleAlbum(album) {
-    return ` <div class="col-sm col-md-2 col-lg-2 d-flex">
-    <a onclick="audio3.play ( )" class="cardHover beatles" href="#"><div class="card">
-      <div class="row">
-        <div class="col">
-          <img
-            class="cardTopImg"
-            src="${album.cover}"
-            class="img-fluid"
-            alt=""
-          />
-        </div>
-        <div class="col colText">
-          <div class="cardText">${album.title}</div>
-        </div>
+    return ` <div class="col-sm d-flex">
+    <a class="cardBg" href="artist.html"><div class="card" style="width: 100px !important">
+      <img
+        class="card-img-top-recently"
+        src="/Assets/falloutboy.png"
+        alt="Card image cap"
+      />
+      <div class="card-body-recently">
+        <h5 class="card-title">Fall Out Boy</h5>
+        <p class="card-text-recently">Greatest Hits</p>
       </div>
-    </div>
+    </div></a>
+  </div>`;
+}
+
+function AlbumsRow(title, albumsHTML) {
+    return `  <div class="container recentlyPlayedTxt">
+    <h7>${title}</h7>
   </div>
-</a>`;
+  <div class="container-fluid-recently recentlyPlayedOptions">
+    <div class="row row-cols-8">
+      
+        ${albumsHTML}
+      
+    </div>
+  </div>`;
 }
 
 window.onload = function() {
 
-    // searchDeezer("Metallica");
-    // searchDeezer("Behemoth");
-    // searchDeezer("Eminem");
+    searchDeezer("Metallica");
+    searchDeezer("Behemoth");
+    searchDeezer("Eminem");
   
     const renderLink = document.querySelector("#render");
     renderLink.addEventListener('click', function() {
         alert("Render is clicked!");
-    });
-
-};
+        let pageContent = document.querySelector(".mainSection");
+        let pageContentHTML = "";
+        pageContent.childNodes.forEach((node, index) => {
+            if (index !== 1) {
+              node.remove();
+            }
+          });
+          albums.forEach((albumResult) => {
+            let rowContent = "";
+            const title = albumResult.title;
+            const data = albumResult.albums;
+      
+            data.forEach((result) => {
+              const title = result.title_short;
+              const cover = result.album.cover_medium;
+              const id = result.album.id;
+              const album = { cover, title, id };
+              rowContent += SingleAlbum(album);
+            });
+            pageContentHTML += AlbumsRow(title, rowContent);
+            rowContent = "";
+          });
+          pageContent.innerHTML += pageContentHTML;
+        });
+      };
 
 /** <div class="mainSection">
     <div class="container-fluid">
